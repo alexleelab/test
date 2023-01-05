55e16e543842eea8e151489c196b96c27b378fa7
alexleelab <alexleelab15@gmail.com>
2023-01-05 21:02:23 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloClient, InMemoryCache, ApolloProvider, split, HttpLink,
+} from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
+const httpLink = new HttpLink({
+  uri: 'https://testpdfbackend.herokuapp.com/graphql',
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://https://testpdfbackend.herokuapp.com/graphql',
+}));
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
